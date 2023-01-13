196e6248b876496312c983d1bf57a9fae569c757
Bhanu <bhnprtp795@gmail.com>
2023-01-13 23:37:59 +0530

    components,props, propsTypes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-    <nav>
-      <li>Home</li>
-      <li>About</li>
-      <li>Contact</li>
-    </nav>
-    <div className="blank">Lovely</div>
+    <Navbar title="TextUtils"/>
     </>
     
   );
