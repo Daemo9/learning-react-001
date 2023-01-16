ea421c482adfaa6dec43c0da1343ba4ed5c39e47
Bhanu <bhnprtp795@gmail.com>
2023-01-16 23:13:49 +0530

    added new component About js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,6 +7,16 @@ export default function TextForm(props) {
         let upperText = text.toUpperCase();
         setText(upperText)
     }
+    const handleLoClick=()=>{
+        // console.log('handleUpClick called..ToUpperCase '+text);
+        let upperText = text.toLowerCase();
+        setText(upperText)
+    }
+
+    const handleClearClick=()=>{
+        setText('')
+    }
+
     const handleOnChange=(event)=>{
         // console.log('handleOnChange called..');
         setText(event.target.value)
@@ -19,11 +29,15 @@ export default function TextForm(props) {
         <div className="mb-3">
             <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
         </div> 
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+        <button className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
     </div>
     <div className="container my-3">
         <h1>Your Text Summary</h1>
         <p>{text.split(" ").length} words and {text.length} characters.</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
     </div>
     </>
   )
